7b923e0cea63b4c7639d61f9482a8cb0b3a3a597
tsuyoshiwada <v23.b.wada@gmail.com>
2017-07-25 14:54:30 +0900

    wip: field props

--- src/__tests__/mockContext.spec.js
+++ src/__tests__/mockContext.spec.js
@@ -4,21 +4,9 @@ import { dripFormField } from 'react-drip-form';
 import { shallow } from 'enzyme';
 import noop from '../internal/noop';
 import mockContext from '../mockContext';
+import { silentError } from './utils';
 
 const Field = dripFormField()(() => <div />);
-const silentError = () => {
-  const original = console.error;
-  const error = jest.fn();
-
-  console.error = error;
-
-  return {
-    error,
-    restore: () => {
-      console.error = original;
-    },
-  };
-};
 
 
 describe('mockContext()', () => {
